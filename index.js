6d39d256d1d565f42318dc28d319aae5f3cb91ca
chungo1998prime <jagomezc@unah.hn>
2025-10-31 01:28:09 -0600

    Creación del servidor inicial con Node y Express

---
+++ index.js
@@ -0,0 +1,12 @@
+
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => res.send('API Productos OK'))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`)
+})
