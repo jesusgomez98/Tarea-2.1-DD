19f609c25069df22ace202b954d326d20966cc04
chungo1998prime <jagomezc@unah.hn>
2025-10-31 01:40:25 -0600

    Creación del servidor inicial con Node y Express

--- index.js
+++ index.js
@@ -10,3 +10,5 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`)
 })
+
+console.log("Servidor escuchando correctamente...")
